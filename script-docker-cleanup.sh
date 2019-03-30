810b8fe41d804a225a845dfb2920c722322e9b87
Ricardo Grassi <grassiricardorg@gmail.com>
2019-03-30 10:02:37 -0300

    fix script docker cleanup

--- script-docker-cleanup.sh
+++ script-docker-cleanup.sh
@@ -19,8 +19,8 @@ echo "remove all unused volumes"
 docker run -v /var/run/docker.sock:/var/run/docker.sock -v /var/lib/docker:/var/lib/docker --rm scripts/docker-cleanup-volumes
 
 echo "send confirmation to slack"
-curl -X POST --data-urlencode 'payload={"channel": "#general", "text": "Run Clear Docker To EC2 production-api", "icon_emoji": ":white_check_mark:"}' https://hooks.slack.com/services/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
+curl -X POST --data-urlencode 'payload={"channel": "#reports", "text": "Run Clear Docker To EC2 sithis-api", "icon_emoji": ":white_check_mark:"}' https://hooks.slack.com/services/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
 
 sudo chmod 777 scripts/docker-cleanup.sh
 
-* 2 * * * ./scripts/docker-cleanup.sh
+0 2 * * * ./scripts/docker-cleanup.sh
